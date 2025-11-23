5b8e80d75e97c043f06e9942e3c983ebb5dcbddb
Andradit <drakushev@mail.ru>
2025-11-23 16:52:50 +0300

    Add changes 23.11.2025

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -17,9 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from .views import home
+from .views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home, name='home'),
+    path('', index, name='index'),
 ]
--- task_manager/views.py
+++ task_manager/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template.loader import render_to_string
 
 
-def home(request):
-    return HttpResponse("task_manager")
+def index(request):
+    return render(request, 'base.html')
+
